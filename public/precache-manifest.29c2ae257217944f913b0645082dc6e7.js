0308dba7a816cfc01ed06aea7eb6404cd4a17f60
neos55555 <778007921@qq.com>
2020-06-19 14:57:05 +0800

    新增public，方便linux下载

---
+++ public/precache-manifest.29c2ae257217944f913b0645082dc6e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c0a43e35000c0da3e1d32837a2452d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53374e9be68dd0bfd9d8",
+    "url": "/static/css/2.12f85f92.chunk.css"
+  },
+  {
+    "revision": "384c9bd7fdadfe463e58",
+    "url": "/static/css/main.67bcafc2.chunk.css"
+  },
+  {
+    "revision": "53374e9be68dd0bfd9d8",
+    "url": "/static/js/2.a3a123f9.chunk.js"
+  },
+  {
+    "revision": "ffb6264c616b204da97e33ad024b6411",
+    "url": "/static/js/2.a3a123f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384c9bd7fdadfe463e58",
+    "url": "/static/js/main.e858babc.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "f198a369946cc3374b1c5b3e8dd630dd",
+    "url": "/static/media/black-hole.f198a369.png"
+  }
+]);
