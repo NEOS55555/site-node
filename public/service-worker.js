d9799ce14a76fa71a9a5bfa255fa1f6c6027d5a5
neos55555 <778007921@qq.com>
2020-06-22 14:49:40 +0800

    前端修改

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e01c8e746ef74ec10568898cf8c0ee4.js"
+  "/precache-manifest.159919b95f9932e05c1da5e35da42149.js"
 );
 
 self.addEventListener('message', (event) => {
