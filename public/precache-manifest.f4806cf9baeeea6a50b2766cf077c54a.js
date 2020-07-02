f1f0e213f996bb907b204cef94f9bf2a751950d2
neos55555 <778007921@qq.com>
2020-07-02 16:33:05 +0800

    123

---
+++ public/precache-manifest.f4806cf9baeeea6a50b2766cf077c54a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a890b44a8ee99eecb70144ef35faa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3b2e5425265433737d",
+    "url": "/static/css/2.12f85f92.chunk.css"
+  },
+  {
+    "revision": "b5d1c4a1210a026e2a8e",
+    "url": "/static/css/main.b8616cca.chunk.css"
+  },
+  {
+    "revision": "5c3b2e5425265433737d",
+    "url": "/static/js/2.7ea62cdd.chunk.js"
+  },
+  {
+    "revision": "ffb6264c616b204da97e33ad024b6411",
+    "url": "/static/js/2.7ea62cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d1c4a1210a026e2a8e",
+    "url": "/static/js/main.ecc73a49.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "f198a369946cc3374b1c5b3e8dd630dd",
+    "url": "/static/media/black-hole.f198a369.png"
+  }
+]);
